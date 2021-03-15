fdbaa3b1a5be9e2d976d96e46f715ac75e40f242
Varun Bhat <seetkipeti@gmail.com>
2021-03-15 19:07:50 +0000

    Removed service provider

--- Assignment2ServicePublisher/app/routes/matMul.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    const matMulController = require('../controllers/matMul.controller.js');
-
-    app.get('/matMul', matMulController.matMulPage)
-
-    app.post('/matMul',matMulController.performMatMul)
-
-}
